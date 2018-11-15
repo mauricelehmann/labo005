7bda2678c7736a7b173ac7a6fc6c17acf6ce0069
mauricelehmann <m.lehmann94@gmail.com>
2018-11-15 08:31:37 +0100

    Ajouté l'entete et le main

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/* ---------------------------
+ Laboratoire    : 5
+ Fichier        : main.cpp
+ Auteur(s)      : Maurice Lehmann, Mahé Fuentes, Harold Redard-Jacot
+ Date           : 15.11.2018
+
+ But            : Affiche le calendrier d'une année entre 1600 et 3000.
+                  L'utilisateur peut choisir de quel jour la semaine commence
+ Remarque(s)    :
+
+ Compilateur    : MinGW-g++ 6.3.0
+ --------------------------- */
+ #include <cstdlib>
+ #include <iostream>
+ #include <string>
+ using namespace std;
+
+
+
+ int main(){
+
+
+
+
+
+     return EXIT_SUCCESS;
+ }
