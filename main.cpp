6482590924b46e22284fbd1f985683b81b4007c8
mauricelehmann <m.lehmann94@gmail.com>
2018-11-15 09:15:10 +0100

    ajouté la todo liste

--- main.cpp
+++ main.cpp
@@ -15,7 +15,22 @@
  #include <string>
  using namespace std;
 
+/*
 
+Ecrivez une fonction qui reçoit en paramètres un message de question, un message d'erreur, et les bornes min et max à vérifier, et boucle jusqu'à ce que l'utilisateur entre une entrée correcte.
+
+Ecrivez une fonction qui affiche une chaine de caractères centrée dans une colonne de largeur donnée
+
+Ecrivez une fonction qui détermine si une année est bissextile.
+
+Ecrivez une fonction qui détermine quel jour de la semaine est le premier janvier d'une année donnée. Il existe une formule mathématique permettant de le déterminer.
+
+Par contre, ne calculez pas quel jour de la semaine est le premier des autres mois. Determinez ce jour à partir de vos boucles.
+
+Ecrivez une fonction qui affiche un mois donné et qui prend entre autres un paramètre spécifiant le jour de la semaine du premier du mois. Cette fonction doit également donner en sortie le premier jour du mois suivant
+
+
+ */
 
  int main(){
 
